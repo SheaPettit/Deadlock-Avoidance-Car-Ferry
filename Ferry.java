0fce83c85265b61b65cb5f56ce872d564dcd9c54
SheaPettit <spettit2@horizon.csueastbay.edu>
2026-04-26 01:31:57 -0700

    Ferry complete: Might need to add some logic on making the program halt to make sure all cars left

--- Ferry.java
+++ Ferry.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 public class Ferry implements Runnable {
   private Semaphore carsBoarding;
+  private Semaphore carsLeaving;
   private final Semaphore toIslandCapacity;
   private final Semaphore toMainlandCapacity;
   private final Semaphore islandLot;
@@ -11,9 +11,11 @@ public class Ferry implements Runnable {
   public Ferry() {
     inboundLocation = CarLocation.MAINLAND;
     carsBoarding = new Semaphore(0, true);
+    carsLeaving = new Semaphore(0, true);
     toIslandCapactiy = new Semaphore(0, true);
     toMainlandCapacity = new Semaphore(0, true);
     islandLot = new Semaphore(50, true);
+    mainland = new Semaphore(0, true);
   }
 
   public CarLocation requestRide(CarLocation carLoc){
@@ -54,18 +56,49 @@ public class Ferry implements Runnable {
   public CarLocation endRide(CarLocation location) {
     if(location != CarLocation.AT_SEA)
       return CarLocation.ERROR;
-    try {
-      islandLot.acquire();
-    } catch (InterruptedException e){
-      e.printStackTrace();
+    if(inboundLocation == CarLocation.ISLAND){
+      try {
+        islandLot.acquire();
+      } catch (InterruptedException e){
+        e.printStackTrace();
+      }
+    }else if(inboundLocation == CarLocation.MAINLAND){
+      try {
+        mainland.acquire();
+      } catch (InterruptedException e){
+        e.printStackTrace();
+      }
     }
-    return inboundLocation;
+    location = inboundLocation;
+    carsLeaving.release();
+    return location;
   }
 
   public void run() {
     ThreadLocalRandom rand = ThreadLocalRandom.current();
+    int numCars = 0;
+    int currentIslandLot;
     while(true){
       Thread.sleep(rand.nextInt(2000,3001));
+      if(inboundLocation == CarLocation.MAINLAND){
+        mainland.release(numCars);
+        carsLeaving.acquire(numCars);
+        currentIslandLot = islandLot.availablePermits();
+        numCars = Math.min(20, Math.min(toIslandCapacity.getQueueLength(), currentIslandLot));
+        islandLot.acquire(currentIslandLot);
+        toIslandCapacity.release(numCars);
+      } else if(inboundLocation == CarLocation.ISLAND){
+        islandLot.release(currentIslandLot);
+        carsLeaving.acquire(numCars);
+        numCars = Math.min(20, toMainlandCapacity.getQueueLength());
+        toMainlandCapacity.release(numCars);
+      }
+      carsBoarding.acquire(numCars);
+      if(inboundLocation == CarLocation.MAINLAND){
+        inboundLocation = CarLocation.ISLAND;
+      } else if(inboundLocation == CarLocation.ISLAND){
+        inboundLocation = CarLocation.MAINLAND;
+      }
     }
   }
 }
